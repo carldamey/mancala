0c3833818203a4cdb4b6b6c50f07c509c1600137
Carl Damey <carldamey@gmail.com>
2023-08-07 11:42:32 -0700

    adds beginning of html and css layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("js working")
+/*----- constants -----*/
+const COLORS = {
+    "0": "black",
+    "1": "red",
+    "1": "blue",
+}
+
+/*----- state variables -----*/
+let winner, turn, board
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init()
+
+function init() {
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
+}
