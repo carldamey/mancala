5f48721458f392071fea3263f5a628a7fe55be0c
Carl Damey <carldamey@gmail.com>
2023-08-07 15:15:45 -0700

    adds function for converting the selected pocket into an index the board array can use

--- js/main.js
+++ js/main.js
@@ -25,9 +25,20 @@ function init() {
 
 function checkPocket(pocket) {
     // console.log(pocket.id[pocket.id.length - 1]) this will check which pocket
-    if (pocket.id[0] === player.toString()[0]) {
-        console.log("valid")
+    if (pocket.id[0] === player.toString()[0]){
+        /* I hope this doesn't violate MVC principles by checking the html data rather than js. I figured this looks a lot cleaner than
+        checking each player against each half of the board index, which is the only other method I can think of currently within my skillset.
+        If this is serious bad practice, brace yourself, I'm about to do it again. */
+        console.log(pocket.innerText)
+        handleTurn(pocket)
     }
+}
+
+function handleTurn(pocket) {
+    let pocketIdx = 0
+    if (player === -1) pocketIdx += 6
+    pocketIdx += Number(pocket.id[pocket.id.length -1])
+    player *= -1
 }
 
 function render() {
