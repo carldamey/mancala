0bea286643fe74d71876f3240e96140eae7d0c7a
Carl Damey <carldamey@gmail.com>
2023-08-07 13:27:49 -0700

    adds system for allowing players to only interact with their own  pockets

--- js/main.js
+++ js/main.js
@@ -6,17 +6,30 @@ const COLORS = {
 }
 
 /*----- state variables -----*/
-let winner, turn, board
+let winner, player, board
 
 /*----- cached elements  -----*/
-
+const pocketsEl = document.getElementById("pockets")
 
 /*----- event listeners -----*/
-
+pocketsEl.addEventListener("click", (evt) => checkPocket(evt.target))
 
 /*----- functions -----*/
 init()
 
 function init() {
-    board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
+    board = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,]
+    player = 1
+    turn = 1
+}
+
+function checkPocket(pocket) {
+    // console.log(pocket.id[pocket.id.length - 1]) this will check which pocket
+    if (pocket.id[0] === player.toString()[0]) {
+        console.log("valid")
+    }
+}
+
+function render() {
+    return
 }
