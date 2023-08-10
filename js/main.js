5ede28459316df01379359ca4b580798b7a9266e
Carl Damey <carldamey@gmail.com>
2023-08-09 19:52:33 -0700

    adds restart button that displays upon the game's end and calls the init function when clicked

--- js/main.js
+++ js/main.js
@@ -13,18 +13,24 @@ const pocketsEl = document.getElementById("pockets")
 const p1MancalaEl = document.getElementById("p1Mancala")
 const p2MancalaEl = document.getElementById("p2Mancala")
 const headerEl = document.querySelector("h1")
+const restartButtonEl = document.getElementById("restart")
 
 /*----- event listeners -----*/
 pocketsEl.addEventListener("click", (evt) => checkPocket(evt.target))
+restartButtonEl.addEventListener("click", init)
 
 /*----- functions -----*/
 init()
 
 function init() {
+    winner = 0
     board = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,]
     mancalas = [0, 0]
     player = 1
     turn = 1
+    restartButtonEl.style.visibility = "hidden"
+    headerEl.innerText = "RED'S TURN"
+    render()
 }
 
 function checkPocket(pocket) {
@@ -41,24 +47,26 @@ function handleTurn(pocketIdx) {
     // check for winner and tie
     let i = 0
     const selectedPocket = pocketIdx
-
-    
-
-
-
     while (board[selectedPocket] > 0) {
         i++
         if (pocketIdx + i === 12) {
             if (player === -1) {
                 board[selectedPocket] --
                 mancalas[1] ++
-            }      
+                if (board[selectedPocket] === 0) {
+                    player *= -1
+                }
+            }
             i = 0
             pocketIdx = 0
         }
         if (pocketIdx + i === 6 && player === 1) {
                 board[selectedPocket] --
                 mancalas[0] ++
+                if (board[selectedPocket] === 0) {
+                    player *= -1
+                    // add feedback displaying extra turn, maybe highlight your mancala and fade 
+                }
         }
         if (board[selectedPocket] > 0) {
             board[selectedPocket] --
@@ -68,7 +76,7 @@ function handleTurn(pocketIdx) {
     p1Pockets = board.slice(0, 6)
     p2Pockets = board.slice(6)
 
-    if (p1Pockets.every((pocket) => pocket === 0) || p1Pockets.every((pocket) => pocket === 0)) {
+    if (p1Pockets.every((pocket) => pocket === 0) || p2Pockets.every((pocket) => pocket === 0)) {
         endGame()
     }
 
@@ -99,7 +107,6 @@ function render() {
         if (player === 1) {headerEl.innerText = "RED'S TURN"}
         else headerEl.innerText = "BLUE'S TURN"
     } else {
-        console.log("render recognizes winner")
         if (winner === 1) {
             headerEl.innerText =  "RED WINS"
         } else if (winner === -1) {
@@ -107,7 +114,8 @@ function render() {
         } else if (winner === 2) {
             headerEl.innerText = "TIE"
         }
-        //display both scores in some neat fashion
+        //display both scores in some neat fashion, maybe replace the pockets with it and include the replay button at the bottom of it
+        restartButtonEl.style.visibility = "visible"
     }
 
 
@@ -117,17 +125,13 @@ function render() {
 }
 
 function endGame() {
-    console.log("endgame function triggered")
     mancalas[0] += p1Pockets.reduce((a, b) => a+b)
     mancalas[1] += p2Pockets.reduce((a, b) => a+b)
     if (mancalas[0] > mancalas[1]) {
         winner = 1
-        console.log("winner func player 1")
     } else if (mancalas[1] > mancalas [0]) {
         winner = -1
-        console.log("winner func player 2")
     } else if (mancalas[0] === mancalas[1]) {
         winner = 2
-        console.log("winner func tie")
     }
 }
